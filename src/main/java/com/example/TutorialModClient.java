7290e6a444e797454f5c7be8c8e3733528086308
BensPlaza <seacreeperz@gmail.com>
2023-09-17 15:43:10 -0500

    Initial commit

---
+++ src/main/java/com/example/TutorialModClient.java
@@ -0,0 +1,9 @@
+package com.example;
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
